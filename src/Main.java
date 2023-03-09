1de2307e0fa4284c4e13a8fb453f40f750288478
IdaRoos <ida_r@live.se>
2023-03-09 14:07:32 +0100

    Uppdaterat API

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
      URL url = new URL ("https://api.wheretheiss.at/v1/satellites/25544");
      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-     // Hämta data och koppla API:t
+     // Sättta upp HTTPRequest inställningar // Hämta data och koppla API:t
         conn.setRequestMethod("GET"); // Sätter request method till GET för att hämta datan
         conn.connect(); // utför kopplingen till API:t
 
@@ -60,20 +60,27 @@ public class Main {
         }else{
             System.out.println("Koppling misslyckades");
         }
-        StringBuilder strData = new StringBuilder();
-        Scanner scanner = new Scanner(url.openStream());
+
+        // Skapa StrBuilder och Scan object
+        StringBuilder strData = new StringBuilder(); // Tar data och bygger upp det som strängvärden
+        Scanner scanner = new Scanner(url.openStream()); // url.openStream() som källa för scannerna
+       // Bygger upp str ned ISS dataSå länge det finns fler data kvar.
         while (scanner.hasNext()) {
             strData.append(scanner.nextLine());
         }
+        // stänger kopplingen så fort while loopen är klar/false
         scanner.close();
 
-        JSONObject dataObject = (JSONObject) new JSONParser().parse(String.valueOf(strData));
+        // Skapar JSONObject av fetched data
+        JSONObject issData = (JSONObject) new JSONParser().parse(String.valueOf(strData));
 
-        String latitudeObj = (String) dataObject.get("latitude").toString();
-        String longitudeObj = (String) dataObject.get("longitude").toString();
-        String nameObj = (String) dataObject.get("name");
+        // Värdena kan även sparas ner som string variabler
+        String latitudeObj = (String) issData.get("latitude").toString();
+        String longitudeObj = (String) issData.get("longitude").toString();
+        String nameObj = (String) issData.get("name");
 
-        System.out.println(dataObject);
+        System.out.println(issData);
+        System.out.println(issData.get("velocity")+ " " + issData.get("altitude"));
         System.out.println("Name: " + nameObj + " Latitude: " + latitudeObj + " Longitude: " + longitudeObj);
     }
 };
